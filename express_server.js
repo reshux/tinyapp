674092277609901aa96882a23d23b3e901360f3f
reshux <duruhanunsal@gmail.com>
2019-03-12 17:55:46 +0000

    Form submission part 1

--- express_server.js
+++ express_server.js
@@ -1,15 +1,16 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-function urlParser(input){
-  var parsed = JSON.parse(input)
-  return parsed
+function generateRandomString() {
+  return Math.random().toString(36).substring(2,6)
 }
 
 app.set("view engine", "ejs")
@@ -25,6 +26,11 @@ app.get("/urls", (req, res) => {
   });
 });
 
+app.get("/urls/new", (req, res) => {
+  var templateVars = urlDatabase
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", {
@@ -38,6 +44,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
